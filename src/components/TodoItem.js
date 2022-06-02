32d93378bcc901318b69912815731f1b28de2427
tranthangduc <tranthangduc0603@gmail.com>
2022-06-02 08:45:21 +0000

    Change Todo status

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -4,9 +4,20 @@
 　・チェックボックスにチェックが入っているか管理する
 　・チェックボックスにチェックが入っているかアイテムをグレーアウトする
 */
-function TodoItem(  ) {
+function TodoItem({ item, onCheck }) {
+  const handleChange = () => {
+    onCheck(item);
+  }
   return (
     <label className="panel-block">
+      <input
+        type="checkbox"
+        checked={item.done}
+        onChange={handleChange}
+      />
+      <span className={item.done ? 'has-text-grey-light' : ''}>
+        {item.text}
+      </span>
     </label>
   );
 }
